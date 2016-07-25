f6f04b1c7a579861ebe9752a2eb637ab0a41882d
Shivam Agrawal <shivamagrawal@Shivams-MacBook-Air.local>
2016-07-24 23:47:42 -0400

    fixed user signup and login

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ var config = require('../config');
 
 var countries = require('country-data').countries;
 
+var unverifiedUsersDB = require('../helpers/dbInterface')('unverifiedUsers');
 var db = require('../helpers/dbInterface')('users');
 var ObjectId = require('mongodb').ObjectID;
 
@@ -15,117 +16,101 @@ var jwt = require('jsonwebtoken');
 var jwtSecret = config.jwtSecret;
 var nativeNumber = config.nativeNumber;
 
-// TODO better user stuff. Currently, only by phone number, but what if phone number switches
-
-// TODO, add area code - do on android
-
-// Authentication: 2 way with phone number & password
-// If number is recylced, no worries, b/c still have password, and later email verif.
-// right now, no good (all messed up). need to do password stuff and need to check for phone SO MUCH TODO.
-
-/*
-
-Login System: A 2 way authentication system using phone number and password
-Users can signup using a phone number and a password. The phone number is verified.
-If a user signs up with a number that already exists in our database, and this user's number is verified,
-then the other user with the same number is removed from the database, to maintain a 1 to 1 ratio between users and phone numbers.
-Can also login using phone number and password.
-
-*/
-
-/*
-
-TODO: add password reset using same system as verification. Use phone number, get text verification number, and input new password in twice
-(like all services do) and input verification number. If verification number matches, password updated :)
-
-*/
+var _ = require('underscore');
+var e = require('../helpers/error');
 
 // TODO: logout: Set verified to false
 
 router.post('/login', function(req, res, next) {
+	
 	var phoneNumber = req.body.phoneNumber;
+	var countryISO = req.body.countryISO.toUpperCase();
 	var password = req.body.password;
-	var countryISO = req.body.countryISO;
-	if (phoneNumber && password) {
-		db.get({ 
-			phoneNumber: encryption.encrypt(phoneNumber)
-		}, function(err, doc) {
-			if (doc) {
-				if (doc.password == encryption.encrypt(password)) {
-					// user has correct credentials: phone number and password
-					doc.verficationCode = verficationGen.generate({ length: 6, charset: 'numeric' });
-					db.update({ phoneNumber: encryption.encrypt(phoneNumber) }, doc, function (err, numReplaced) {
-						if (err) { res.send('Error something'); }
-						else {
-							/*sendText(phoneNumber, doc.verficationCode, function(sent) {
-								if (sent) res.send(JSON.stringify(doc));
-								else res.send('Error sending message');
-							});*/
-						}
-					});
-				} else { res.send('Incorrect password'); }
-			} else if (err) { res.send('Unknown error'); }
-			else { res.send('No account with this number exists');  }
-		});
-	} else { res.send('No phone number and/or password provided'); }
+
+	var internationalPhoneNumber = countries[countryISO].countryCallingCodes[0] + phoneNumber;
+
+	db.get({ 
+		phoneNumber: encryption.encrypt(internationalPhoneNumber)
+	}, function(err, doc) {
+		if (!(_.isEmpty(doc))) {
+			if (doc.password == encryption.encrypt(password)) {
+
+				unverifiedUsersDB.put({
+					phoneNumber: encryption.encrypt(internationalPhoneNumber),
+					password: encryption.encrypt(password),
+					verficationCode: verficationGen.generate({ length: 6, charset: 'numeric' })
+				}, function(success, doc) {
+					if (success) {
+						sendText(internationalPhoneNumber, doc.verficationCode, function(sent) {
+							if (sent) res.send(JSON.stringify({ userID: doc._id }));
+							else res.send(e.new(500, 'Error sending verification text'));
+						});
+					} else { res.send(e.new(500, 'Something went wrong')); }
+				});
+			} else { res.send(e.new(400, 'Incorrect password')); }
+		} else { res.send(e.new(500, 'Something went wrong')); }
+	});
+
 });
 
 router.post('/signup', function(req, res, next) {
 
 	var phoneNumber = req.body.phoneNumber;
-	var countryISO = req.body.countryISO;
+	var countryISO = req.body.countryISO.toUpperCase();
 	var password = req.body.password;
 
-	var internationalPhoneNumber = phoneNumber;//countries[countryISO.toUpperCase()].countryCallingCodes[0] + phoneNumber;
-	if (typeof phoneNumber != 'undefined' && typeof password != 'undefined') {
-		db.put({
-			phoneNumber: encryption.encrypt(internationalPhoneNumber),
-			password: encryption.encrypt(password),
-			verficationCode: verficationGen.generate({ length: 6, charset: 'numeric' }),
-			groups: []
-		}, function(success, doc) {
-			if (success) {
-				console.log(doc);
-				/*sendText(internationalPhoneNumber, doc.verficationCode, function(sent) {
-					if (sent) res.send(JSON.stringify(doc));
-					else res.send('Error sending message');
-				});*/
-				res.send(JSON.stringify(doc));
-			} else { res.send('Unknown error'); }
-		});
-	} else { res.send('No phone number and/or password provided'); }
+	var internationalPhoneNumber = countries[countryISO].countryCallingCodes[0] + phoneNumber;
+
+	unverifiedUsersDB.put({
+		phoneNumber: encryption.encrypt(internationalPhoneNumber),
+		password: encryption.encrypt(password),
+		verficationCode: verficationGen.generate({ length: 6, charset: 'numeric' }),
+	}, function(success, doc) {
+		if (success) {
+			sendText(internationalPhoneNumber, doc.verficationCode, function(sent) {
+				if (sent) res.send(JSON.stringify({ userID: doc._id }));
+				else res.send(e.new(500, 'Error sending verification text'));
+			});
+		} else { res.send(e.new(500, 'Something went wrong')); }
+	});
+
 });
 
 router.post('/verify', function(req, res, next) {
-	// replace existing account if it exists - TODO: need to delete other instances of existing user in groups, etc.
+
 	var userID = req.body.userID;
 	var verficationCode = req.body.verficationCode;
-	var phoneNumber = req.body.phoneNumber; //countries[req.body.countryISO.toUpperCase()].countryCallingCodes[0].concat(req.body.phoneNumber);
-	if (verficationCode && userID) {
-		db.get({ _id: ObjectId(userID) }, function (success, doc) {
-			if (success && doc) {
-				if (verficationCode == doc.verficationCode) {
-					delete doc.verficationCode;
-					db.update({ _id: ObjectId(userID) }, doc, function (success, doc) {
-						if (success) {
-							// removes all user accounts with this phone number except for the one that was just verified
-							// in the future, instead of being removed, they will be archived with email identification so people can recover their account
-							db.remove({ phoneNumber: encryption.encrypt(phoneNumber), _id: { $ne: ObjectId(userID) } }, function(success) {});
-							res.send(JSON.stringify({ token: jwt.sign({ userID: doc._id, phoneNumber: doc.phoneNumber }, jwtSecret) }));
-						} else { res.send('error updating'); }
-					});
-				} else { res.send('incorrect code'); }
-			} else if (!success) { res.send('Unknown error'); }
-			else { res.send('Account does not exist'); }
-		});
-	} else { res.send('Missing userID and/or verficationCode'); }
+
+	unverifiedUsersDB.get({ _id: ObjectId(userID) }, function (success, doc) {
+		if (success && !(_.isEmpty(doc))) {
+			if (verficationCode == doc.verficationCode) {
+				delete doc.verficationCode;
+				doc.groups = [];
+				db.put(doc, function (success, doc) {
+					if (success) {
+						unverifiedUsersDB.remove({ _id: ObjectId(userID) }, function(success) {});
+						db.remove({ phoneNumber: doc.phoneNumber, _id: { 
+							$ne: ObjectId(userID) } 
+						}, function(success) {});
+						res.send(JSON.stringify({ 
+							token: jwt.sign({ 
+								userID: doc._id, 
+								phoneNumber: doc.phoneNumber 
+							}, jwtSecret) 
+						}));
+					} else { res.send(e.new(500, 'Error verifying account')); }
+				});
+			} else { res.send(e.new(400, 'Incorrect verification code')); }
+		} else { res.send(e.new(500, 'Something went wrong')); }
+	});
+
 });
 
 function sendText(phoneNumber, verficationCode, callback) {
 	twilio.messages.create({
 		to: phoneNumber,
 		from: nativeNumber,
-		body: 'Verification Code: ' + verficationCode + '. Ignore this if you are already verified (someone may be trying to hack your account).',
+		body: 'PictureUs verification Code: ' + verficationCode,
 	}, function (err, message) {
 		if (err) { console.log(err); callback(false); }
 		else { callback(true); }
